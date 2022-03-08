fc68a88e245a6216902314baa805657e2f552dd2
surajvdya <surajvdya@gmail.com>
2022-03-08 02:04:57 -0500

    adding spring

---
+++ src/test/java/com/pipeline20/pipeline20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline20.pipeline20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
